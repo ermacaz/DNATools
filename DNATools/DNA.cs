9f85bb5ce51b882f1fabbb41b5a96e43e767c09c
Matt Hamada <mattahamada@gmail.com>
2013-07-28 13:51:19 -0700

    Initial commit

---
+++ DNATools/DNA.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNATools
+{
+    class DNA
+    {
+        private string sequence;
+        public string Sequence { get; set; }
+        
+        public DNA(string newSeq)
+        {
+            Sequence = newSeq.ToLower();
+        }
+
+        public DNA Complement()
+        {
+            string origin = Sequence;
+            origin = origin.Replace('a', 'b');
+            origin = origin.Replace('t', 'a');
+            origin = origin.Replace('b', 't');
+            origin = origin.Replace('g', 'b');
+            origin = origin.Replace('c', 'g');
+            origin = origin.Replace('b', 'c');
+            
+            return new DNA(origin);
+        }
+    }
+}
